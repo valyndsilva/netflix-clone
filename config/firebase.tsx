e60720fce7ef21ba0bc3b62977552c353d6e533d
valyndsilva <valyndsilva@gmail.com>
2022-11-09 06:17:41 +0000

    getApp

--- config/firebase.tsx
+++ config/firebase.tsx
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 // import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
