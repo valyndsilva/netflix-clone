d83adf4ede57f78d94cc01ba27bca29fc5cbd297
valyndsilva <valyndsilva@gmail.com>
2022-11-09 05:51:14 +0000

    firebase getApp()

--- config/firebase.tsx
+++ config/firebase.tsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-// import { initializeApp, getApp, getApps } from "firebase/app";
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
@@ -19,10 +19,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 const auth = getAuth();
 
-export default app;
 export { auth, db };
+export default app;
