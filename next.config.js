09894c7a31543351981a0eb3c28d897bd961c34e
valyndsilva <valyndsilva@gmail.com>
2022-11-11 14:09:12 +0000

    netflix cleanup

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const withTM = require("next-transpile-modules")([
   "@stripe/firestore-stripe-payments",
 ]); // pass the modules you would like to see transpiled
 
-// const repo = "netflix-clone";
-// const assetPrefix = `/${repo}/`;
-// const basePath = `/${repo}`;
 const nextConfig = withTM({
   reactStrictMode: true,
   swcMinify: true,
@@ -18,8 +15,6 @@ const nextConfig = withTM({
   // time in seconds of no pages generating during static
   // generation before timing out
   staticPageGenerationTimeout: 2000,
-  // assetPrefix,
-  // basePath,
 });
 
 module.exports = nextConfig;
